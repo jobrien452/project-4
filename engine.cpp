b464f8aa45782af3a8be56203e6f4fdf9094e3c7
Jake O'Brien <jakeo8@vt.edu>
2015-11-17 00:54:59 -0500

    created barebones engine code, added more to engine.h

---
+++ engine.cpp
@@ -0,0 +1,11 @@
+#include "engine.h"
+
+Engine :: Engine(){
+	p1 = new Racket();
+	p2 = new Racket();
+	b = new Ball();
+}
+
+void Engine :: update(QPainter * p){
+
+}
--- engine.h
+++ engine.h
@@ -3,9 +3,11 @@
 
 #include <QObject>
 #include <QRect>
+#include <QPainter>
 
 #include "racket.h"
 #include "ball.h"
+//canvas to record score???
 
 class Engine : public QObject {
 
@@ -13,6 +15,22 @@ class Engine : public QObject {
     
     public:
         Engine();
+	void update(QPainter*); 
         //create signals for model to update in real time
         //define view bounds, add hard coded size for ball and 
         //rackets in cpp file.
+        //connect qtimer to view controller and put update method in qwidget
+        //paint method
+    public slots:
+	void reset();
+	void quit();
+	
+    private:
+	Racket p1, p2;
+	Ball b;
+	int s1,s2;
+
+
+};
+
+#endif 
