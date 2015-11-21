f743afa9887c35bd9c79000c884c39a00977e112
Jake O'Brien <jakeo8@vt.edu>
2015-11-20 20:04:26 -0500

    created main, todo add cmake, start debugging

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "model.h"
+#include "view.h"
+#include "engine.h"
+
+int main(int argc, char **argv){
+
+    QApplication app (argc, argv);
+
+    Model myModel(800, 600);
+    Engine gameEngine(myModel);
+    View myView(0, myModel);
+    myView.show();
+    return app.exec();
+}
